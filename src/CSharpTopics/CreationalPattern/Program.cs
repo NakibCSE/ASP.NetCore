67b6bb50e25fa091d1754b247920001c72ffdbad
Nakib <nakkibcse@gmail.com>
2025-04-25 20:59:45 +0600

    practice creational pattern

---
+++ src/CSharpTopics/CreationalPattern/Program.cs
@@ -0,0 +1,20 @@
+﻿using CreationalPattern.Builder;
+using CreationalPattern.Singleton;
+using System.Text;
+
+Logger logger =  Logger.GetLogger();
+Logger logger2 = Logger.GetLogger();
+
+//Using our created builder
+ItemBuilder itemBuilder = new ItemBuilder();
+itemBuilder.SetValue3("Password");
+itemBuilder.SetValue1("UserName");
+
+Item item = itemBuilder.GetItem();
+
+//Using built in builder
+StringBuilder stringBuilder = new StringBuilder();
+stringBuilder.AppendLine("Password");
+stringBuilder.AppendLine("UserName");
+
+string text = stringBuilder.ToString();
---
+++ src/CSharpTopics/CreationalPattern/Protype/Car.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreationalPattern.Protype
+{
+    public class Car
+    {
+        public string Model { get; set; }
+        public string Speed { get; set; }
+        public double Fuel { get; set; }
+
+        public Car Copy()
+        {
+            return new Car { Fuel = Fuel, Model = Model };
+        }
+    }
+}
