c3d5f860302d906f15bb13a45e10661fbcfe1f0d
Nakib <nakkibcse@gmail.com>
2025-04-28 16:50:12 +0600

    Added new entity book and author, also added dbcontext class

---
+++ src/Demo/Demo.Domain/Entities/Book.cs
@@ -0,0 +1,9 @@
+﻿namespace Demo.Domain.Entities
+{
+    public class Book : IEntity<Guid>
+    {
+        public Guid ID { get; set; }
+        public string Title { get; set; }
+        public Guid AuthorID { get; set; }
+    }
+}
