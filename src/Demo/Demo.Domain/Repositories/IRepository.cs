e381cf5850087a821ca8a5ee2c7ea6aa9e95f902
Nakib <nakkibcse@gmail.com>
2025-04-29 16:58:01 +0600

    File updated

--- src/Demo/Demo.Domain/Repositories/IRepository.cs
+++ src/Demo/Demo.Domain/Repositories/IRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Demo.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Demo.Domain.Repositories
 {
-    public interface IRepository
+    public interface IRepository<T, G> where T : IEntity<G>
     {
 
     }
