5e20f6b4ad5d06d37c85b10c305f35efc63bc5fd
Nakib <nakkibcse@gmail.com>
2025-04-29 16:55:56 +0600

    Clean architecture practice

--- src/Demo/Demo.Infrastructure/ApplicationDbContext.cs
+++ src/Demo/Demo.Infrastructure/ApplicationDbContext.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Demo.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace Demo.Infrastructure
 {
     public class ApplicationDbContext : DbContext
     {
-
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Author> Authors { get; set; }
     }
 }
