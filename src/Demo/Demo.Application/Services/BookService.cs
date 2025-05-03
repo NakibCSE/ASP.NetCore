6b4925b1f00bc0288b0dd297a817c1c4fbc4df9f
Nakib <nakkibcse@gmail.com>
2025-05-03 16:53:29 +0600

    Practice clean architecture

---
+++ src/Demo/Demo.Application/Services/BookService.cs
@@ -0,0 +1,18 @@
+﻿using Demo.Domain.Entities;
+using Demo.Domain.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.Application.Services
+{
+    public class BookService : IBookService
+    {
+        public void AddBook(Book book)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Demo/Demo.Web/Areas/Admin/Controllers/BooksController.cs
@@ -0,0 +1,29 @@
+﻿using Demo.Web.Areas.Admin.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Demo.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class BooksController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Add()
+        {
+            var model = new AddBookModel();
+            return View(model);
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public IActionResult Add(AddBookModel model)
+        {
+            if (ModelState.IsValid)
+            {
+
+            }
+            return View(model);
+        }
+    }
+}
