5e3a6b15b71fd81e3f671f0178c95fd3bae5bdfb
Nakib <nakkibcse@gmail.com>
2025-05-28 09:44:20 +0600

    Identity classes added

---
+++ src/Demo/Demo.Infrastructure/Identity/ApplicatoinRoleManager.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.Infrastructure.Identity
+{
+    public class ApplicatoinRoleManager 
+        : RoleManager<ApplicationRole>
+    {
+        public ApplicatoinRoleManager(IRoleStore<ApplicationRole> store,
+           IEnumerable<IRoleValidator<ApplicationRole>> roleValidators,
+           ILookupNormalizer keyNormalizer, IdentityErrorDescriber errors,
+           ILogger<RoleManager<ApplicationRole>> logger)
+           : base(store, roleValidators, keyNormalizer, errors, logger)
+        {
+
+        }
+    }
+}
