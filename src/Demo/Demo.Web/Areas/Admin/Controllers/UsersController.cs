61d454796c0b3b867a5698433e67d34b3e6bf168
Nakib <nakkibcse@gmail.com>
2025-06-16 18:09:27 +0600

    Role base practice

---
+++ src/Demo/Demo.Web/Areas/Admin/Controllers/UsersController.cs
@@ -0,0 +1,85 @@
+﻿using Demo.Infrastructure;
+using Demo.Infrastructure.Identity;
+using Demo.Web.Areas.Admin.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Demo.Web.Areas.Admin.Controllers
+{
+    [Area("Admin"), Authorize(Roles = "Admin")]
+    public class UsersController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IUserStore<ApplicationUser> _userStore;
+        private readonly IUserEmailStore<ApplicationUser> _emailStore;
+
+        public UsersController(UserManager<ApplicationUser> userManager,
+            IUserStore<ApplicationUser> userStore)
+        {
+            _userManager = userManager;
+            _userStore = userStore;
+
+        }
+
+        public IActionResult AddUserAsync()
+        {
+            var model = new AddUserModel();
+            return View(model);
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddUserAsync(AddUserModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var user = CreateUser();
+
+                    await _userStore.SetUserNameAsync(user, model.Email, CancellationToken.None);
+                    await _emailStore.SetEmailAsync(user, model.Email, CancellationToken.None);
+
+                    user.RegistrationDate = DateTime.UtcNow;
+
+                    var result = await _userManager.CreateAsync(user, model.Password);
+
+                    await _userManager.AddToRoleAsync(user, model.Role);
+
+                    TempData.Put("ResponseMessage", new ResponseModel
+                    {
+                        Message = "User added",
+                        Type = ResponseTypes.Success
+                    });
+                }
+                catch (Exception ex)
+                {
+                    var message = "User Create Failed";
+                    ModelState.AddModelError("UserCreateFailed", message);
+                    TempData.Put("ResponseMessage", new ResponseModel
+                    {
+                        Message = message,
+                        Type = ResponseTypes.Danger
+                    });
+                }
+            }
+
+            return View(model);
+        }
+
+        private ApplicationUser CreateUser()
+        {
+            try
+            {
+                return Activator.CreateInstance<ApplicationUser>();
+            }
+            catch
+            {
+                throw new InvalidOperationException($"Can't create an instance of '{nameof(ApplicationUser)}'. " +
+                    $"Ensure that '{nameof(ApplicationUser)}' is not an abstract class and has a parameterless constructor, or alternatively " +
+                    $"override the register page in /Areas/Identity/Pages/Account/Register.cshtml");
+            }
+        }
+    }
+}
---
+++ src/Demo/Demo.Web/Data/Migrations/20250616073222_AddRoleSeedData.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Demo.Web.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRoleSeedData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { new Guid("23fe4e81-5015-42a0-8d76-d1f08c6b227a"), "2025-04-19 01:02:03", "HR", "HR" },
+                    { new Guid("5e76c76d-4cf6-4784-8796-fd9f5c8cf29d"), "2025-04-19 01:02:01", "Admin", "ADMIN" },
+                    { new Guid("b7a80d7e-24d8-4cc3-8981-ee4e1b5b1b3a"), "2025-04-19 01:02:04", "Author", "AUTHOR" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: new Guid("23fe4e81-5015-42a0-8d76-d1f08c6b227a"));
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: new Guid("5e76c76d-4cf6-4784-8796-fd9f5c8cf29d"));
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: new Guid("b7a80d7e-24d8-4cc3-8981-ee4e1b5b1b3a"));
+        }
+    }
+}
