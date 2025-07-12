06dcee198404921192d474397eb8f8afc58b8c2d
Nakib <nakkibcse@gmail.com>
2025-07-12 08:16:46 +0600

    AWS practice

---
+++ src/Demo/AWSExamples/Program.cs
@@ -0,0 +1,31 @@
+﻿using Amazon;
+using Amazon.SQS;
+using Amazon.SQS.Model;
+
+RegionEndpoint ServiceRegion = RegionEndpoint.USEast1;
+
+IAmazonSQS client = new AmazonSQSClient(ServiceRegion);
+var url = "https://sqs.us-east-1.amazonaws.com/847888492411/aspnetb11";
+var body = "Hello from Bangladesh.";
+
+await SendMessage(client, url, body, null);
+
+static async Task<SendMessageResponse> SendMessage(
+            IAmazonSQS client,
+            string queueUrl,
+            string messageBody,
+            Dictionary<string, MessageAttributeValue> messageAttributes)
+{
+    var sendMessageRequest = new SendMessageRequest
+    {
+        DelaySeconds = 10,
+        MessageAttributes = messageAttributes,
+        MessageBody = messageBody,
+        QueueUrl = queueUrl,
+    };
+
+    var response = await client.SendMessageAsync(sendMessageRequest);
+    Console.WriteLine($"Sent a message with id : {response.MessageId}");
+
+    return response;
+}
